bf5094ff9596ea15298b37a8f81d2d78745f4cfe
jacob-tabak <jacob@tabak.me>
2014-09-21 14:33:47 -0400

    Remove unnecessary call to getBodyAs()

--- app/src/main/java/com/timehop/droidcon2014retrofitsample/data/foursquare/api/FoursquareErrorHandler.java
+++ app/src/main/java/com/timehop/droidcon2014retrofitsample/data/foursquare/api/FoursquareErrorHandler.java
@@ -7,7 +7,7 @@ public class FoursquareErrorHandler implements ErrorHandler {
   @Override
   public Throwable handleError(RetrofitError cause) {
     if (cause.getResponse() != null && cause.getSuccessType() == FoursquareResponse.class) {
-      FoursquareResponse response = (FoursquareResponse) cause.getBodyAs(cause.getSuccessType());
+      FoursquareResponse response = (FoursquareResponse) cause.getBody();
       if (response.getMeta().getErrorDetail() != null) {
         return new FoursquareException(response.getMeta().getErrorDetail(), cause);
       }
